08d6e79c3014ec4fa936000b906cba0ad143a95b
folksgl <Gfolks14@gmail.com>
2021-05-20 10:15:04 -0400

    Improve clarity of install scripts purpose

---
+++ install_prometheus.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# To update prometheus, update the download URL and checksum here
+prometheus_download_url="https://github.com/prometheus/prometheus/releases/download/v2.27.1/prometheus-2.27.1.linux-amd64.tar.gz"
+prometheus_sha256sum="ce637d0167d5e6d2561f3bd37e1c58fe8601e13e4e1ea745653c068f6e1317ae  prometheus.tar.gz"
+
+download_output_file="prometheus.tar.gz"
+
+# Download the Prometheus archive
+wget --quiet --output-document "$download_output_file" "$prometheus_download_url"
+
+# Compare sha256sum to known value at https://prometheus.io/download/
+download_hash="$(sha256sum "$download_output_file")"
+
+if [[ "$download_hash" != "$prometheus_sha256sum" ]]; then
+    echo "sha256sum failed validation"
+    exit 1
+fi
+
+# Extract the archive to the current directory, preserving the existing prometheus.yml
+tar -xzf "$download_output_file" --strip-components 1 --skip-old-files
+rm "$download_output_file"
