24808fb352eeedb6ceecd257172038b7a113e0f3
folksgl <Gfolks14@gmail.com>
2021-05-20 09:41:57 -0400

    Move download url to file and reference it in setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Download the Prometheus archive
-wget --quiet -O prometheus.tar.gz https://github.com/prometheus/prometheus/releases/download/v2.27.1/prometheus-2.27.1.linux-amd64.tar.gz
+wget --quiet -O prometheus.tar.gz --input-file=prometheus_download_url.txt
 
 valid_prometheus_shasum="ce637d0167d5e6d2561f3bd37e1c58fe8601e13e4e1ea745653c068f6e1317ae  prometheus.tar.gz"
 if [[ "$(sha256sum prometheus.tar.gz)" !=  "$valid_prometheus_shasum" ]]; then
