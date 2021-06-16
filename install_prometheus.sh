ffd6a5d98d8695417266152ee9a74a3421f430ac
folksgl <Gfolks14@gmail.com>
2021-06-16 14:52:04 -0400

    Simplify hash check logic

--- install_prometheus.sh
+++ install_prometheus.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 set -e
 
+download_output_file="prometheus.tar.gz"
+
 # To update prometheus, update the download URL and checksum below.
 # New versions can be found at https://prometheus.io/download/
-prometheus_download_url="https://github.com/prometheus/prometheus/releases/download/v2.27.1/prometheus-2.27.1.linux-amd64.tar.gz"
-prometheus_sha256sum="ce637d0167d5e6d2561f3bd37e1c58fe8601e13e4e1ea745653c068f6e1317ae  prometheus.tar.gz"
+download_url="https://github.com/prometheus/prometheus/releases/download/v2.27.1/prometheus-2.27.1.linux-amd64.tar.gz"
+valid_hash="ce637d0167d5e6d2561f3bd37e1c58fe8601e13e4e1ea745653c068f6e1317ae  $download_output_file"
 
 # Download the Prometheus archive
-download_output_file="prometheus.tar.gz"
-wget --quiet --output-document "$download_output_file" "$prometheus_download_url"
+wget --quiet --output-document "$download_output_file" "$download_url"
 
 # Compare sha256sum
-download_hash="$(sha256sum "$download_output_file")"
-
-if [[ "$download_hash" != "$prometheus_sha256sum" ]]; then
-    echo "sha256sum failed validation"
-    exit 1
-fi
+echo "$valid_hash" | sha256sum --check --quiet
 
 # Extract the archive to the current directory, preserving the existing prometheus.yml
 tar -xzf "$download_output_file" --strip-components 1 --skip-old-files
