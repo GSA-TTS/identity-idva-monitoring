310bc8af1519131a7c010a18d6554021ed36b43e
folksgl <Gfolks14@gmail.com>
2021-09-20 12:17:23 -0400

    Use internal alerts domain name for cluster peering

--- alertmanager/start-alertmanager.sh
+++ alertmanager/start-alertmanager.sh
@@ -5,21 +5,4 @@ if [ -z "$ENVIRONMENT_NAME" ]; then
     exit
 fi
 
-echo "My IP is: $CF_INSTANCE_INTERNAL_IP"
-
-if [[ $CF_INSTANCE_INDEX == 0 ]]; then
-    # HA leader node. Peers will specify this node in their --cluster.peer list
-    ./alertmanager --web.listen-address="0.0.0.0:$PORT"
-else
-    # Additional HA instances
-    sleep 10 # Allow time for leader to launch
-
-    # host performs a dns lookup for the domain name. grep & awk pull out the addess returned
-    peer_list=$(host identity-idva-monitoring-dev.apps.internal | grep address | awk '{ print $4 }' | sed 's/$/:9094 /' | sed 's/^/--cluster.peer=/')
-    peer_list=$(echo -e "$peer_list" | tr -d '\n')
-    echo "Peer list: $peer_list"
-
-    sleep 5 # Give a small amount of buffer time for all additional HA instances to complete looking up leader ip
-
-    ./alertmanager --web.listen-address="0.0.0.0:$PORT" "$peer_list"
-fi
+./alertmanager --web.listen-address="0.0.0.0:$PORT" --cluster.peer="identity-idva-monitoring-alerts-$ENVIRONMENT_NAME.apps.internal:9094"
