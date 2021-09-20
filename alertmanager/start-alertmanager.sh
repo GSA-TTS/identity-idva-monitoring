19d0ccd6ccb0e9858e1de3540b895ed24d6331e0
folksgl <Gfolks14@gmail.com>
2021-09-20 13:15:15 -0400

    Add web-config.yml generation for self-signed tls setup

--- alertmanager/start-alertmanager.sh
+++ alertmanager/start-alertmanager.sh
@@ -1,8 +1,9 @@
 ./install_alertmanager.sh
+../create_web_config.sh
 
 if [ -z "$ENVIRONMENT_NAME" ]; then
     echo "Error: empty ENVIRONMENT_NAME environment variable."
     exit
 fi
 
-./alertmanager --web.listen-address="0.0.0.0:$PORT" --cluster.peer="identity-idva-monitoring-alerts-$ENVIRONMENT_NAME.apps.internal:9094"
+./alertmanager --web.listen-address="0.0.0.0:$PORT" --cluster.peer="identity-idva-monitoring-alerts-$ENVIRONMENT_NAME.apps.internal:9094" --web.config.file=web-config.yml
