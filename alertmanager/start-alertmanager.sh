10b2bfc58606bfe07f82d5548752c3b351540054
Garrett Folks <garrett.folks@gsa.gov>
2021-09-21 15:04:22 -0400

    Alertmanager tls setup (#38)
    * Remove process section for prometheus manifest
    
    * Add tls settings for all scrape configs
    
    * Add web-config file generation script
    
    * Remove start-alertmanager.sh script
    
    * Add .gitignore to ignore generated config files

--- alertmanager/start-alertmanager.sh
+++
@@ -1,8 +0,0 @@
-./install_alertmanager.sh
-
-if [ -z "$ENVIRONMENT_NAME" ]; then
-    echo "Error: empty ENVIRONMENT_NAME environment variable."
-    exit
-fi
-
-./alertmanager --web.listen-address="0.0.0.0:$PORT" --cluster.peer="identity-idva-monitoring-alerts-$ENVIRONMENT_NAME.apps.internal:9094"
