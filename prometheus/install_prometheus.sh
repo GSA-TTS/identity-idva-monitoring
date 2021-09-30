3abcae77a0cfd4f9846b75c14f258bc0ec650c68
Garrett Folks <garrett.folks@gsa.gov>
2021-09-30 16:29:11 -0400

    Bump Prometheus version from 2.30.0 -> 2.30.1 (#42)

--- prometheus/install_prometheus.sh
+++ prometheus/install_prometheus.sh
@@ -5,8 +5,8 @@ download_output_file="prometheus.tar.gz"
 
 # To update prometheus, update the download URL and checksum below.
 # New versions can be found at https://prometheus.io/download/
-download_url="https://github.com/prometheus/prometheus/releases/download/v2.30.0/prometheus-2.30.0.linux-amd64.tar.gz"
-valid_hash="49c0809d4983f91c9afb8d260b36b821e90a6dcb82d0bad605ff9a3102a9e6d8  $download_output_file"
+download_url="https://github.com/prometheus/prometheus/releases/download/v2.30.1/prometheus-2.30.1.linux-amd64.tar.gz"
+valid_hash="9c542a653bf2f17043056ca13ea428e56ff2f677ee03bf7494aa2efe2b8329a3  $download_output_file"
 
 # Download the Prometheus archive
 wget --quiet --output-document "$download_output_file" "$download_url"
