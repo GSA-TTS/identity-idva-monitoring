17c6d648a1f6f8ca0de9fd67cb7df0f2c2dec0f1
folksgl <Gfolks14@gmail.com>
2021-10-01 10:53:14 -0400

    Remove manual hash validation
    Prior to this change, updating the version of Prometheus or Alertmanager
    meant changing the version number in several places in the installation
    script as well as manually providing the hash for the tar files (see
    https://github.com/18F/identity-idva-monitoring/pull/45/files). With
    this change, users can update the version_num variable in one place at
    the top of the script, and the URLs for the tar and the shasum file are
    built by the script. This eliminates the possibility for human error and
    typos and will always result in the version specified being installed.

--- alertmanager/install_alertmanager.sh
+++ alertmanager/install_alertmanager.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e
 
-download_output_file="alertmanager.tar.gz"
+# To update alertmanager, simply update the version number below
+version_num="0.23.0"
+download_url="https://github.com/prometheus/alertmanager/releases/download/v$version_num"
+archive_name="alertmanager-$version_num.linux-amd64.tar.gz"
 
-# To update alertmanager, update the download URL and hash below.
-download_url="https://github.com/prometheus/alertmanager/releases/download/v0.23.0/alertmanager-0.23.0.linux-amd64.tar.gz"
-valid_hash="77793c4d9bb92be98f7525f8bc50cb8adb8c5de2e944d5500e90ab13918771fc  $download_output_file"
+archive_url="$download_url/$archive_name"
+shasum_url="$download_url/sha256sums.txt"
 
-# Download the Prometheus archive
-wget --quiet --output-document "$download_output_file" "$download_url"
+# Download the Alertmanager archive and shasum file
+wget --quiet "$archive_url"
+wget --quiet "$shasum_url"
 
 # Compare sha256sum
-echo "$valid_hash" | sha256sum --check --quiet
+sha256sum --check --ignore-missing --status sha256sums.txt
 
 # Extract the archive to the current directory, preserving the existing prometheus.yml
-tar -xzf "$download_output_file" --strip-components 1 --skip-old-files
-rm "$download_output_file"
+tar -xzf $archive_name --strip-components 1 --skip-old-files
+rm "$archive_name" sha256sums.txt
