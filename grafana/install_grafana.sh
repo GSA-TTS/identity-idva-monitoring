1f1e6554e8b6f418eabe9aebe802ef04b5338854
dzaslavskiy <dzaslavskiy@easydynamics.com>
2021-11-29 12:04:48 -0500

    Add auto-update to grafana

--- grafana/install_grafana.sh
+++ grafana/install_grafana.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
+version_num=$(cat VERSION)
 download_output_file="grafana.tar.gz"
-
-# To update grafana, update the download URL and checksum below.
-# New versions can be found at https://grafana.com/grafana/download
-# in the 'Standalone Linux Binaries' section.
-download_url="https://dl.grafana.com/oss/release/grafana-8.2.1.linux-amd64.tar.gz"
-valid_hash="e238ec1f78369fc8f9c094584223d763a6e86a83cf33bcb7b0d750341dc2e988  $download_output_file"
+download_url="https://dl.grafana.com/oss/release/grafana-$version_num.linux-amd64.tar.gz"
+# Get hash
+valid_hash="$(curl -s "$download_url.sha256" | tr -d '\n') $download_output_file"
 
 # Download the grafana archive
 wget --quiet --output-document "$download_output_file" "$download_url"
