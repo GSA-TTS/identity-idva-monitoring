a031aa23cce2ed489ade1cc5cd7f0fbf6ad93a46
folksgl <Gfolks14@gmail.com>
2021-12-10 15:59:53 -0500

    Update script comments

--- watchtower/install_watchtower.sh
+++ watchtower/install_watchtower.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# To update Cortex, simply update the version number below
+# To update Watchtower, simply update the version number in the VERSION file
 version_num=$(cat VERSION)
 download_url="https://github.com/18F/watchtower/releases/download/v$version_num/"
 binary_name="watchtower-$version_num.linux-amd64"
@@ -10,14 +10,14 @@ shasum_filename="sha256sums.txt"
 archive_url="$download_url/$archive_name"
 shasum_url="$download_url/$shasum_filename"
 
-# Download the Prometheus archive and shasum file
+# Download the Watchtower archive and shasum file
 wget --quiet "$archive_url"
 wget --quiet "$shasum_url"
 
 # Compare sha256sum
 sha256sum --check --ignore-missing --status "$shasum_filename"
 
-# Add executable permissions
+# Extract and clean up
 tar -xzf "$archive_name"
 rm "$archive_name" "$shasum_filename"
 mv "$binary_name" watchtower
