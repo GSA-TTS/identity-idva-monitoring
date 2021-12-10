5c1b4c9ed73f709ff4dd78a2cb40ce87c96b117f
folksgl <Gfolks14@gmail.com>
2021-12-10 15:08:37 -0500

    Add watchtower tool setup

---
+++ watchtower/install_watchtower.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+# To update Cortex, simply update the version number below
+version_num=$(cat VERSION)
+download_url="https://github.com/18F/watchtower/releases/download/v$version_num/"
+binary_name="watchtower-$version_num.linux-amd64"
+archive_name="$binary_name.tar.gz"
+shasum_filename="sha256sums.txt"
+
+archive_url="$download_url/$archive_name"
+shasum_url="$download_url/$shasum_filename"
+
+# Download the Prometheus archive and shasum file
+wget --quiet "$archive_url"
+wget --quiet "$shasum_url"
+
+# Compare sha256sum
+sha256sum --check --ignore-missing --status "$shasum_filename"
+
+# Add executable permissions
+tar -xzf "$archive_name"
+rm "$archive_name" "$shasum_filename"
+mv "$binary_name" watchtower
