e7412968e53582455218542bee8554d3ab7c424d
Garrett Folks <garrett.folks@gsa.gov>
2021-12-10 16:21:56 -0500

    add watchtower deployment (#98)
    * Parameterize shasum filename
    
    * Add watchtower tool setup
    
    * Add route for prometheus to monitor watchtower
    
    * Add Prometheus scrape config for watchtower
    
    * Add basic alerting rules for watchtower metrics
    
    * Lower default memory to 32mb based on 25% usage of 64MB allocation

--- prometheus/install_prometheus.sh
+++ prometheus/install_prometheus.sh
@@ -5,17 +5,18 @@ set -e
 version_num=$(cat VERSION)
 download_url="https://github.com/prometheus/prometheus/releases/download/v$version_num"
 archive_name="prometheus-$version_num.linux-amd64.tar.gz"
+shasum_filename="sha256sums.txt"
 
 archive_url="$download_url/$archive_name"
-shasum_url="$download_url/sha256sums.txt"
+shasum_url="$download_url/$shasum_filename"
 
 # Download the Prometheus archive and shasum file
 wget --quiet "$archive_url"
 wget --quiet "$shasum_url"
 
 # Compare sha256sum
-sha256sum --check --ignore-missing --status sha256sums.txt
+sha256sum --check --ignore-missing --status "$shasum_filename"
 
 # Extract the archive to the current directory, preserving the existing prometheus.yml
 tar -xzf "$archive_name" --strip-components 1 --skip-old-files
-rm "$archive_name" sha256sums.txt
+rm "$archive_name" "$shasum_filename"
