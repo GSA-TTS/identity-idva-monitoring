65a7d0989c9cac82ff90d82e7f0998126612a674
folksgl <Gfolks14@gmail.com>
2022-03-14 16:59:31 -0400

    Switch to using https port for Cortex

--- create-network-policies.sh
+++ create-network-policies.sh
@@ -22,7 +22,7 @@ cf add-network-policy kibana es-proxy
 # Source = Prometheus
 cf add-network-policy prometheus alertmanager
 cf add-network-policy prometheus cf-metrics
-cf add-network-policy prometheus cortex
+cf add-network-policy prometheus cortex --protocol tcp --port 61443
 cf add-network-policy prometheus elasticsearch-metrics
 cf add-network-policy prometheus grafana --port 61443 --protocol tcp
 cf add-network-policy prometheus kong --port 8100 --protocol tcp
