78c277cefc10a8b6044e3ff666f68fb103c36c55
Garrett Folks <Gfolks14@gmail.com>
2022-03-21 14:48:49 -0400

    alertmanager use cloud gov certs (#170)
    * Configure Prometheus to use Cloud.gov https port
    
    * Remove usage of instance identity certs for alertmanager
    
    * Change prometheus -> alertmanager port in network policies
    
    * Correct missing line-continuation in start command
    
    * Remove web_config script

--- alertmanager/create_web_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# See https://docs.cloudfoundry.org/devguide/deploy-apps/instance-identity.html
-# for information on Cloud Foundry Instance Identity Credentials.
-{
-    echo "---"
-    echo "tls_server_config:"
-    echo "  cert_file: $CF_INSTANCE_CERT"
-    echo "  key_file: $CF_INSTANCE_KEY"
-} > web-config.yml
--- create-network-policies.sh
+++ create-network-policies.sh
@@ -13,18 +13,18 @@ cf add-network-policy alertmanager alertmanager --protocol udp --port 9094
 cf add-network-policy elasticsearch-metrics es-proxy --protocol tcp --port 61443
 
 # Source = Grafana
-cf add-network-policy grafana cortex --protocol tcp --port 61443
+cf add-network-policy grafana cortex     --protocol tcp --port 61443
 cf add-network-policy grafana prometheus --protocol tcp --port 61443
 
 # Source = Kibana
 cf add-network-policy kibana es-proxy --protocol tcp --port 61443
 
 # Source = Prometheus
-cf add-network-policy prometheus alertmanager
-cf add-network-policy prometheus cf-metrics --protocol tcp --port 61443
-cf add-network-policy prometheus cortex --protocol tcp --port 61443
-cf add-network-policy prometheus grafana --port 61443 --protocol tcp
+cf add-network-policy prometheus alertmanager          --protocol tcp --port 61443
+cf add-network-policy prometheus cf-metrics            --protocol tcp --port 61443
+cf add-network-policy prometheus cortex                --protocol tcp --port 61443
+cf add-network-policy prometheus grafana               --protocol tcp --port 61443 
 cf add-network-policy prometheus elasticsearch-metrics --protocol tcp --port 61443
-cf add-network-policy prometheus kong --port 8100 --protocol tcp
-cf add-network-policy prometheus redis-metrics --protocol tcp --port 61443
-cf add-network-policy prometheus watchtower --protocol tcp --port 61443
+cf add-network-policy prometheus kong                  --protocol tcp --port 8100
+cf add-network-policy prometheus redis-metrics         --protocol tcp --port 61443
+cf add-network-policy prometheus watchtower            --protocol tcp --port 61443
