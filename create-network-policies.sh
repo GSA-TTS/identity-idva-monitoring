7643ef6be68b8698ab0a6d63d562eba9bd527ea4
Garrett Folks <Gfolks14@gmail.com>
2022-05-09 16:59:24 -0400

    restrict egress (#191)
    * Move alertmanager to public egress space
    
    * Move Cortex install into GitHub Action
    
    * Move Grafana to a closed-egress space type
    
    * Move Prometheus to closed-egress space type
    
    * Move ElasticSearch exporter to closed-egress space type
    
    * Simplify kibana manifest.yaml
    
    * Add spacing to improve network policy script readability

--- create-network-policies.sh
+++ create-network-policies.sh
@@ -19,30 +19,32 @@ closed="$restricted-closed"
 
 cf target -s "$restricted"
 
-# Source = Alertmanager
-cf add-network-policy alertmanager alertmanager --protocol tcp --port 9094 -s "$restricted"
-cf add-network-policy alertmanager alertmanager --protocol udp --port 9094 -s "$restricted"
-
-# Source = Elasticsearch-metrics
-cf add-network-policy elasticsearch-metrics es-proxy --protocol tcp --port 61443 -s "$restricted"
-
-# Source = Grafana
-cf add-network-policy grafana cortex     --protocol tcp --port 61443 -s "$restricted"
-cf add-network-policy grafana prometheus --protocol tcp --port 61443 -s "$restricted"
-
 # Source = Kibana
-cf add-network-policy kibana es-proxy --protocol tcp --port 61443
-
-# Source = Prometheus
-cf add-network-policy prometheus alertmanager          --protocol tcp --port 61443 -s "$restricted"
-cf add-network-policy prometheus cf-metrics            --protocol tcp --port 61443 -s "$restricted"
-cf add-network-policy prometheus cortex                --protocol tcp --port 61443 -s "$restricted"
-cf add-network-policy prometheus grafana               --protocol tcp --port 61443 -s "$restricted"
-cf add-network-policy prometheus elasticsearch-metrics --protocol tcp --port 61443 -s "$restricted"
-cf add-network-policy prometheus kong                  --protocol tcp --port 8100  -s "$closed"
-cf add-network-policy prometheus redis-metrics         --protocol tcp --port 61443 -s "$restricted"
-cf add-network-policy prometheus watchtower            --protocol tcp --port 61443 -s "$closed"
+cf add-network-policy kibana es-proxy                  --protocol tcp --port 61443 -s "$restricted"
 
 cf target -s "$closed"
 
 cf add-network-policy watchtower outbound-proxy        --protocol tcp --port 61443 -s "$public"
+
+# Source = Grafana
+cf add-network-policy grafana cortex                   --protocol tcp --port 61443 -s "$restricted"
+cf add-network-policy grafana prometheus               --protocol tcp --port 61443 -s "$closed"
+
+# Source = Prometheus
+cf add-network-policy prometheus alertmanager          --protocol tcp --port 61443 -s "$public"
+cf add-network-policy prometheus cf-metrics            --protocol tcp --port 61443 -s "$restricted"
+cf add-network-policy prometheus cortex                --protocol tcp --port 61443 -s "$restricted"
+cf add-network-policy prometheus grafana               --protocol tcp --port 61443 -s "$closed"
+cf add-network-policy prometheus elasticsearch-metrics --protocol tcp --port 61443 -s "$closed"
+cf add-network-policy prometheus kong                  --protocol tcp --port 8100  -s "$closed"
+cf add-network-policy prometheus redis-metrics         --protocol tcp --port 61443 -s "$restricted"
+cf add-network-policy prometheus watchtower            --protocol tcp --port 61443 -s "$closed"
+
+# Source = Elasticsearch-metrics
+cf add-network-policy elasticsearch-metrics es-proxy   --protocol tcp --port 61443 -s "$restricted"
+
+cf target -s "$public"
+
+# Source = Alertmanager
+cf add-network-policy alertmanager alertmanager        --protocol tcp --port 9094 -s "$public"
+cf add-network-policy alertmanager alertmanager        --protocol udp --port 9094 -s "$public"
