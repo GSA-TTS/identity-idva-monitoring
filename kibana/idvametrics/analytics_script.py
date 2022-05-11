594e16ed2b358792b2a7169ebb990b72965ae852
Michael Eisenberg <meisenberg@easydynamics.com>
2022-05-11 19:15:15 -0400

    Rename and redfine variables

--- kibana/idvametrics/analytics_script.py
+++ kibana/idvametrics/analytics_script.py
@@ -262,15 +262,78 @@ def send_query_and_evaluate_result(
         process_composite_aggregation_data(query_result, "avg_response_time")
 
 
+<<<<<<< HEAD
+=======
+def get_authorization_header_to_idva_flows(email: str, password: str, login_url: str):
+    """
+    Returns a header, containing an access token, for an http request to flow data.
+    """
+    # the required authentication to obtain an access token
+    auth = {"email": email, "password": password}
+
+    # the required access token for accessing flow data
+    access_token = requests.post(login_url, data=auth).json()["access_token"]
+    return {"Authorization": f"Bearer {access_token}"}
+
+
+def get_mappings(
+    flow_id: str, email: str, password: str, login_url: str, base_url: str
+):
+    """
+    Returns the mappings of flow id to flow name and node id to node names.
+    """
+    header = get_authorization_header_to_idva_flows(email, password, login_url)
+    # pulling out the flow data for a flow of a given flow id
+    flow_url = f"{base_url}/{flow_id}"
+    flow = requests.get(flow_url, headers=header).json()["flowInfo"]
+    # We always run the script with one specified flow id, so flows should always have one element
+    nodes = flow["graphData"]["elements"]["nodes"]
+
+    node_mappings = []
+    for node in nodes:
+        try:
+            # the node title is defined
+            node_title = node["data"]["properties"]["nodeTitle"]["value"]
+        except KeyError:
+            # the node title has not yet been defined
+            continue
+
+        # Only add the node id to title mapping if the title is defined for the node
+        node_mappings.append(
+            {
+                "id": node["id"],
+                "title": node_title,
+            }
+        )
+    ids_names_mapping = {
+        "flow_id": flow_id,
+        "flow_name": flow["name"],
+        "nodes": node_mappings,
+    }
+    return ids_names_mapping
+
+
+>>>>>>> fc0a238... Rename and redfine variables
 cmd_line_parser = argparse.ArgumentParser()
 cmd_line_parser.add_argument("--host")
 cmd_line_parser.add_argument("--port")
 cmd_line_parser.add_argument("--flow_id")
 cmd_line_parser.add_argument("--start_date", default=None)
 cmd_line_parser.add_argument("--end_date", default=None)
+cmd_line_parser.add_argument("--username")
+cmd_line_parser.add_argument("--password")
+cmd_line_parser.add_argument("--login_url")
+cmd_line_parser.add_argument("--base_url")
 arguments = cmd_line_parser.parse_args()
 
 elasticsearch = OpenSearch(hosts=[{"host": arguments.host, "port": arguments.port}])
+mappings = get_mappings(
+    arguments.flow_id,
+    arguments.username,
+    arguments.password,
+    arguments.login_url,
+    arguments.base_url,
+)
 
 send_query_and_evaluate_result(
     query_avg_response_time,
