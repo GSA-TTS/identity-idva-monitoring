b9b461098a4b68ff79ab200f822165585943d6ce
Michael Eisenberg <meisenberg@easydynamics.com>
2022-05-17 12:49:54 -0400

    Update documentation and formatting

--- idvametrics/upload_metrics.py
+++ idvametrics/upload_metrics.py
@@ -1,25 +1,38 @@
+"""
+This file is for analytics scripting, querying data from the dev-skevents-* index pattern
+and placing the result in the dev-analytics-* index pattern for further visualizations
+"""
+
 import argparse
+import sys
 import requests
 from opensearchpy import OpenSearch
 import analyticsconstants
-from analytics_query import *
+from analytics_query import CompositeAggregationQuery, ScanQuery
 import queries
 
+
 def get_command_line_arguments():
-  cmd_line_parser = argparse.ArgumentParser()
-  cmd_line_parser.add_argument("--host")
-  cmd_line_parser.add_argument("--port")
-  cmd_line_parser.add_argument("--flow_id")
-  cmd_line_parser.add_argument("--start_date", default=None)
-  cmd_line_parser.add_argument("--end_date", default=None)
-  cmd_line_parser.add_argument("--username")
-  cmd_line_parser.add_argument("--password")
-  cmd_line_parser.add_argument("--base_url")
-  arguments = cmd_line_parser.parse_args()
+    """
+    Returns command line argument names and values.
+    """
+    cmd_line_parser = argparse.ArgumentParser()
+    cmd_line_parser.add_argument("--host")
+    cmd_line_parser.add_argument("--port")
+    cmd_line_parser.add_argument("--flow_id")
+    cmd_line_parser.add_argument("--start_date", default=None)
+    cmd_line_parser.add_argument("--end_date", default=None)
+    cmd_line_parser.add_argument("--username")
+    cmd_line_parser.add_argument("--password")
+    cmd_line_parser.add_argument("--base_url")
+    arguments = cmd_line_parser.parse_args()
 
-  return arguments
+    return arguments
 
-def get_authorization_header_to_idva_flows(email: str, password: str, login_url: str):
+
+def get_authorization_header_to_idva_flows(
+    email: str, password: str, login_url: str
+) -> dict:
     """
     Returns a header, containing an access token, for an http request to flow data.
     """
@@ -28,16 +41,16 @@ def get_authorization_header_to_idva_flows(email: str, password: str, login_url:
 
     # the required access token for accessing flow data
     json = requests.post(login_url, data=auth).json()
-    
+
     try:
         access_token = json["access_token"]
         return {"Authorization": f"Bearer {access_token}"}
     except KeyError:
         print(json)
+        sys.exit(1)
 
-def get_mappings(
-    flow_id: str, email: str, password: str, base_url: str
-):
+
+def get_mappings(flow_id: str, email: str, password: str, base_url: str) -> dict:
     """
     Returns the mappings of flow id to flow name and node id to node names.
     """
@@ -45,14 +58,19 @@ def get_mappings(
 
     header = get_authorization_header_to_idva_flows(email, password, login_url)
     # pulling out the flow data for a flow of a given flow id
-    flow_url = f"{base_url}/flows/{flow_id}"
-    flow = requests.get(flow_url, headers=header).json()["flowInfo"]
+    flow_url = f"{base_url}/v1/flows/{flow_id}"
+    json = requests.get(flow_url, headers=header).json()
+    try:
+        flow = json["flowInfo"]
+    except KeyError:
+        print(json)
+        sys.exit(1)
     # We always run the script with one specified flow id, so flows should always have one element
     nodes = flow["graphData"]["elements"]["nodes"]
 
     node_mappings = []
     for node in nodes:
-        
+
         try:
             # the node title is defined
             node_title = node["data"]["properties"]["nodeTitle"]["value"]
@@ -74,54 +92,60 @@ def get_mappings(
     }
     return ids_names_mapping
 
-def main():
-  arguments = get_command_line_arguments()
-  elasticsearch = OpenSearch(hosts=[{"host": arguments.host, "port": arguments.port}], timeout=300)
 
-  mappings = get_mappings(
-    arguments.flow_id,
-    arguments.username,
-    arguments.password,
-    arguments.base_url,
-  )
+def main() -> None:
+    """
+    The main method for running the script.
+    """
+    arguments = get_command_line_arguments()
+    elasticsearch = OpenSearch(
+        hosts=[{"host": arguments.host, "port": arguments.port}], timeout=300
+    )
 
-  kwargs = {
-    "elasticsearch": elasticsearch,
-    "index_pattern": analyticsconstants.EVENTS_INDEX_PATTERN,
-    "query": queries.connector_pass_rate,
-    "flow_id": arguments.flow_id,
-    "event_message": "Connector Pass Rate",
-    "metric": "connector_pass_rate",
-    "metric_key": ["flowId", "interactionId", "id", "tsEms"],
-    "num_composite_buckets": analyticsconstants.DEFAULT_NUM_COMPOSITE_BUCKETS,
-    "document_keys": ["id", "connectionId", {"property": "outcomeStatus"}],
-    "start_date": arguments.start_date,
-    "end_date": arguments.end_date,
-    "mappings": mappings
-  }
+    mappings = get_mappings(
+        arguments.flow_id,
+        arguments.username,
+        arguments.password,
+        arguments.base_url,
+    )
 
-  connector_pass_rate = ScanQuery(**kwargs)
-  connector_pass_rate.send_query_and_evaluate_results()
-  kwargs["metric_key"] = ["interactionId", "id"]
-  kwargs["event_message"] = "Connector Response Time"
-  kwargs["metric"] = "connector_response_time"
-  kwargs["query"] = queries.connector_response_time
-  kwargs["index_pattern"] = analyticsconstants.SK_INDEX_PATTERN
-  kwargs["document_keys"] = ["sessionLength"]
+    kwargs = {
+        "elasticsearch": elasticsearch,
+        "index_pattern": analyticsconstants.EVENTS_INDEX_PATTERN,
+        "query": queries.connector_pass_rate,
+        "flow_id": arguments.flow_id,
+        "event_message": "Connector Pass Rate",
+        "metric": "connector_pass_rate",
+        "metric_key": ["flowId", "interactionId", "id", "tsEms"],
+        "num_composite_buckets": analyticsconstants.DEFAULT_NUM_COMPOSITE_BUCKETS,
+        "document_keys": ["id", "connectionId", {"property": "outcomeStatus"}],
+        "start_date": arguments.start_date,
+        "end_date": arguments.end_date,
+        "mappings": mappings,
+    }
 
-  connector_response_time = CompositeAggregationQuery(**kwargs)
-  connector_response_time.send_query_and_evaluate_results()
+    connector_pass_rate = ScanQuery(**kwargs)
+    connector_pass_rate.send_query_and_evaluate_results()
+    kwargs["metric_key"] = ["interactionId", "id"]
+    kwargs["event_message"] = "Connector Response Time"
+    kwargs["metric"] = "connector_response_time"
+    kwargs["query"] = queries.connector_response_time
+    kwargs["index_pattern"] = analyticsconstants.SK_INDEX_PATTERN
+    kwargs["document_keys"] = ["sessionLength"]
 
-  kwargs["metric_key"] = ["interactionId"]
-  kwargs["event_message"] = "Interaction Response Time"
-  kwargs["metric"] = "interaction_response_time"
-  kwargs["query"] = queries.workflow_response_time
-  kwargs["index_pattern"] = analyticsconstants.SK_INDEX_PATTERN
-  kwargs["document_keys"] = ["sessionLength"]
+    connector_response_time = CompositeAggregationQuery(**kwargs)
+    connector_response_time.send_query_and_evaluate_results()
 
-  workflow_response_time = CompositeAggregationQuery(**kwargs)
-  workflow_response_time.send_query_and_evaluate_results()
+    kwargs["metric_key"] = ["interactionId"]
+    kwargs["event_message"] = "Interaction Response Time"
+    kwargs["metric"] = "interaction_response_time"
+    kwargs["query"] = queries.workflow_response_time
+    kwargs["index_pattern"] = analyticsconstants.SK_INDEX_PATTERN
+    kwargs["document_keys"] = ["sessionLength"]
+
+    workflow_response_time = CompositeAggregationQuery(**kwargs)
+    workflow_response_time.send_query_and_evaluate_results()
 
 
 if __name__ == "__main__":
-  main()
+    main()
