ce8629569012a247f168f8f0e7bb2080fe17b9cc
Michael Eisenberg <43248318+mikeisen1@users.noreply.github.com>
2022-05-18 15:28:58 -0400

    Feature/add ids to names mapping (#193)
    * Add ids to name mappings functionality
    
    Support the mapping of flow ids to flow names and
    node ids to node names.
    
    * Add function signature type hints
    
    * Rename and redfine variables
    
    * Import necessary modules
    
    * Pass in one base url to the script
    
    Instead of passing in a separate url each for login and the flow data,
    pass in a single base url from which those two will be constructed.
    
    * Update base url command line argument
    
    * Exit script on unsuccessful HTTP response

--- kibana/idvametrics/analytics_script.py
+++ kibana/idvametrics/analytics_script.py
@@ -5,6 +5,7 @@ and placing the result in the dev-analytics-* index pattern for further visualiz
 
 import argparse
 from datetime import datetime, timedelta
+import requests
 from dateutil import parser
 from opensearchpy import OpenSearch, helpers
 
@@ -262,15 +263,85 @@ def send_query_and_evaluate_result(
         process_composite_aggregation_data(query_result, "avg_response_time")
 
 
+def get_authorization_header_to_idva_flows(email: str, password: str, login_url: str):
+    """
+    Returns a header, containing an access token, for an http request to flow data.
+    """
+    # the required authentication to obtain an access token
+    auth = {"email": email, "password": password}
+
+    # the required access token for accessing flow data
+    json = requests.post(login_url, data=auth).json()
+    try:
+        access_token = json["access_token"]
+        return {"Authorization": f"Bearer {access_token}"}
+    except KeyError:
+        print(json)
+        exit(1)
+
+
+def get_mappings(
+    flow_id: str, email: str, password: str, base_url: str
+):
+    """
+    Returns the mappings of flow id to flow name and node id to node names.
+    """
+    login_url = f"{base_url}/customers/login"
+
+    header = get_authorization_header_to_idva_flows(email, password, login_url)
+    # pulling out the flow data for a flow of a given flow id
+    flow_url = f"{base_url}/v1/flows/{flow_id}"
+    json = requests.get(flow_url, headers=header).json()
+    try:
+        flow = json["flowInfo"]
+    except KeyError:
+        print(json)
+        exit(1)
+    # We always run the script with one specified flow id, so flows should always have one element
+    nodes = flow["graphData"]["elements"]["nodes"]
+
+    node_mappings = []
+    for node in nodes:
+        try:
+            # the node title is defined
+            node_title = node["data"]["properties"]["nodeTitle"]["value"]
+        except KeyError:
+            # the node title has not yet been defined
+            continue
+
+        # Only add the node id to title mapping if the title is defined for the node
+        node_mappings.append(
+            {
+                "id": node["id"],
+                "title": node_title,
+            }
+        )
+    ids_names_mapping = {
+        "flow_id": flow_id,
+        "flow_name": flow["name"],
+        "nodes": node_mappings,
+    }
+    return ids_names_mapping
+
+
 cmd_line_parser = argparse.ArgumentParser()
 cmd_line_parser.add_argument("--host")
 cmd_line_parser.add_argument("--port")
 cmd_line_parser.add_argument("--flow_id")
 cmd_line_parser.add_argument("--start_date", default=None)
 cmd_line_parser.add_argument("--end_date", default=None)
+cmd_line_parser.add_argument("--username")
+cmd_line_parser.add_argument("--password")
+cmd_line_parser.add_argument("--base_url")
 arguments = cmd_line_parser.parse_args()
 
 elasticsearch = OpenSearch(hosts=[{"host": arguments.host, "port": arguments.port}])
+mappings = get_mappings(
+    arguments.flow_id,
+    arguments.username,
+    arguments.password,
+    arguments.base_url,
+)
 
 send_query_and_evaluate_result(
     query_avg_response_time,
