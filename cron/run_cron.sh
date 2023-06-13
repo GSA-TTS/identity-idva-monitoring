3394455149492f6409c63107ff1877205e09e23f
dzaslavskiy <dzaslavskiy@users.noreply.github.com>
2023-06-13 17:25:41 -0400

    fix cron install (#253)
    * fix cron install
    
    * Fix redis exporter install

--- cron/run_cron.sh
+++ cron/run_cron.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e
 
-version_num=$(cat VERSION)
-download_url="https://github.com/aptible/supercronic/releases/download/v$version_num"
-file_name="supercronic-linux-amd64"
-
-wget --quiet "$download_url/$file_name"
-
-# Compare sha1sum
-sha1sum --check sha1sum
-
-mv "$file_name" supercronic
-chmod +x supercronic
-
 username=$(echo "$VCAP_SERVICES" | jq -r '.["cloud-gov-service-account"][0].credentials.username')
 password=$(echo "$VCAP_SERVICES" | jq -r '.["cloud-gov-service-account"][0].credentials.password')
 api=$(echo "$VCAP_APPLICATION" | jq -r '.cf_api')
@@ -23,3 +11,5 @@ cf --version
 cf api "$api"
 cf auth "$username" "$password"
 cf target -o "$org" -s "$space"
+
+./supercronic crontab
